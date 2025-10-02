73d9c2bc3087d17c102c95dbcac7474d89608c77
Nathaniel M. Capule <llss.s.sls.lll@gmail.com>
2025-10-02 17:57:32 +0000

    31/94 exercises

--- exercises/04_primitive_types/primitive_types3.rs
+++ exercises/04_primitive_types/primitive_types3.rs
@@ -1,6 +1,7 @@
 fn main() {
     // TODO: Create an array called `a` with at least 100 elements in it.
     // let a = ???
+    let a = [0; 100];
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
@@ -8,4 +9,5 @@ fn main() {
         println!("Meh, I eat arrays like that for breakfast.");
         panic!("Array not big enough, more elements needed");
     }
+    println!("Array contents: {:?}", a);
 }
